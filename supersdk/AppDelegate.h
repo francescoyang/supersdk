69f2b089dc85cfc4d75bf21a23724c3c433267b5
Tongyue Wang <tongyuewang@tongyues-macbook-air.local>
2015-10-23 09:49:03 +0800

    Initial Commit

---
+++ supersdk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  supersdk
+//
+//  Created by Tongyue Wang on 15/10/23.
+//  Copyright © 2015年 Tongyue Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ supersdk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  supersdk
+//
+//  Created by Tongyue Wang on 15/10/23.
+//  Copyright © 2015年 Tongyue Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
